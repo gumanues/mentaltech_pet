9914939a58ef85860f40b581a66c0e15cb44cf3a
Gustavo <gumanues@gmail.com>
2022-06-29 16:53:55 -0300

    GerarPDF

--- php/gerarPDF.php
+++ php/gerarPDF.php
@@ -1,63 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>PDF</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 // Require composer autoload
-require_once("../vendor/autoload.php");
-require_once("conexaoBanco.php");
+require_once("vendor/autoload.php");
+require_once("conexao.php");
 
-$idCompromisso = $_POST['idComp'];
-$comando = "SELECT * FROM compromissos WHERE idCompromisso=".$idCompromisso;
+$idPDF = $_POST['idPDF'];
+$comando = "SELECT * FROM agendamento A 
+INNER JOIN servicos S on S.id = A.servicos_id
+INNER JOIN funcionarios F ON F.id = A.funcionarios_id
+INNER JOIN pets P ON P.id = A.pets_id
+INNER JOIN clientes C ON C.id = P.clientes_id
+WHERE A.id = '$idPDF'";
+
 $resultado = mysqli_query($conexao, $comando);
 $comp = mysqli_fetch_assoc($resultado);
 // Create an instance of the class:
 $mpdf = new \Mpdf\Mpdf();
 
 $mpdf->WriteHTML('
-<style>
-
-h2,h3,p {
-    font-family: "Arial";
-}
-
-h3 {
-    color: #222;
-}
-
-
-</style>
-
+<table>
+<tr>
+  <td><p class="card-text"><b>Data de cadastro: </b>'.$comp['data'].'</p></td>
+</tr>
+<tr>
+    <td><p class="card-text"><b>Horário: Das </b>'.$comp['horarioInicio'].'<b> até as </b>'.$comp['horarioFinal'].'</p></td>
+</tr>
+</table>
+<table>
+<tr>
+  <td><p class="card-text"><b>Cliente: </b>'.$comp['nomeCompleto'].'</p></td>
+</tr>
+<tr>
+  <td><p class="card-text"><b>Telefone: </b>'.$comp['telefone'].'</p></td>
+</tr>
+</table>
+<table>
+<tr>
+  <td><p class="card-text"><b>Raça: </b>'.$comp['raca'].'</p></td>
+</tr>
+<tr>
+    <td><p class="card-text"><b>Nome do Pet: </b>'.$comp['nome'].'</p></td>
+</tr>
+<tr>
+  <td><p class="card-text"><b>Serviço: </b>'.$comp['descricao'].'</p></td>
+</tr>
+<tr>
+  <td><p class="card-text"><b>Funcionário dirigente: </b>'.$comp['nomeCompleto'].'</p></td>
+</tr>
+</table>
 ');
 
-// Write some HTML code:
-$mpdf->WriteHTML('<h1> Informação de compromisso</h1>');
-$mpdf->WriteHTML('<hr>');
 
-$mpdf->WriteHTML('<h3>Dados Gerais</h3>');
-$mpdf->WriteHTML('<p>Descrição: '.$comp['descricao'].'</p>');
-$mpdf->WriteHTML('<p>Data Início: '. $comp['dataInicio'].'</p>');
-$mpdf->WriteHTML('<p>Hora: '.$comp['hora'].'</p>');
-$mpdf->WriteHTML('<p>Data Finalização: '.$comp['dataFim'].' </p> <br>');
-
-$mpdf->WriteHTML('<h3>Localização</h3>');
-$mpdf->WriteHTML('<p>Local: '. $comp['local'].'</p>');
-$mpdf->WriteHTML('<p>Rua: '.$comp['rua'].'</p>');
-$mpdf->WriteHTML('<p>Número: '.$comp['numero'].' </p>');
-$mpdf->WriteHTML('<p>Bairro: '.$comp['bairro'].' </p>');
-$mpdf->WriteHTML('<p>Cidade: '.$comp['cidade'].' </p> <br>');
-
-
-
-$mpdf->WriteHTML('<h3>Pessoas</h3>');
-$comando="SELECT p.nome, p.sobrenome FROM pessoas p INNER JOIN compromissos_pessoas cp ON p.idPessoa=cp.pessoas_idPessoa WHERE compromissos_idCompromisso=".$idCompromisso;
-
-    $resultado=mysqli_query($conexao,$comando);
-
-    $pessoas=array();
-
-    while($p = mysqli_fetch_assoc($resultado)){
-        array_push($pessoas,$p);
-    }
-    foreach($pessoas as $p){
-        $mpdf->WriteHTML("<p>".$p['nome']." ".$p['sobrenome']."</p>");
-    }
     $mpdf->Output();
 
 ?>
